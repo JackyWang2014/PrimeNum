113b9be9a4e1a8ec009cf876a533ed362ec1bf76
WangQitai <wangqitai@wangqitaidemacbook-pro.local>
2016-05-21 17:28:16 +0800

    Initial Commit

---
+++ PrimeNum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PrimeNum
+//
+//  Created by WangQitai on 16/5/21.
+//  Copyright © 2016年 WangQitai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
