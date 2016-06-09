fe811164c703bfce44139d1b9a386c443166a86e
WangQitai <wangqitai@WangQitaideMacBook-Pro.local>
2016-06-09 12:44:55 +0800

    First initial PrimeNum

--- PrimeNum/main.c
+++ PrimeNum/main.c
@@ -7,9 +7,29 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+void printPrime(int n)
+{
+    int a = sqrt(n);
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            break;
+        }
+        if (i == (int)sqrt(n)) {
+            printf("%d ",n);
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//    int n = 9;
+//    int a = sqrt(n);
+//    printf("n = %d a = %d",n,a);
+    
+    for (int i = 4; i < 100; i++) {
+        printPrime(i);
+    }
+    
+    
     return 0;
 }
